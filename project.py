4a2aebe8d5b867a98fd76dc942d2b2497bc767d8
Shubham Garg <shubham.garg@aricent.com>
2019-07-26 09:27:22 +0530

    adding lines to project

--- project.py
+++ project.py
@@ -1,81 +1,208 @@
 def A():
     print("entering inside A")
     print("calling API B from A")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     B()
 
 def B():
     print("entering inside B")
     print("calling API C")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     C()
 
 def C():
     print("entering inside C")
     print("calling API D")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     D()
 
 def D():
     print("entering inside D")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
 
 def E():
     print("entering inside E")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     F()
 
 def F():
     print("entering inside F")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     G()
 def G():
     print("entering inside G")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     H()
 
 def H():
     print("entering inside H")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     I()
 def I():
     print("entering inside I")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     J()
 
 def J():
     print("entering inside J")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
 
 def K():
     print("entering inside K")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     L()
 
 def L():
     print("entering inside L")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     M()
 
 def M():
     print("entering inside M")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     N()
 
 def N():
     print("entering inside N")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     O()
 
 def O():
     print("entering inside O")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
 
 def P():
     print("entering inside D")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     Q()
 
 def Q():
     print("entering inside Q")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     R()
 
 def R():
     print("entering inside R")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     S()
+
 def S():
     print("entering inside S")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     T()
 def T():
     print("entering inside T")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
     Q()
 
 def Q():
     print("entering inside Q")
+    #line3
+    #line4
+    #line5
+    #line6
+    #line7
+    #line8
 
 
 def main():
