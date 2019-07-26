2b4b961c98399493a1bb546fe6a99ae1b2f2e11c
Shubham Garg <shubham.garg@aricent.com>
2019-07-26 06:50:29 +0530

    creating project

---
+++ project.py
@@ -0,0 +1,78 @@
+def A():
+    print("entering inside A")
+    B()
+
+def B():
+    print("entering inside B")
+    C()
+
+def C():
+    print("entering inside C")
+    D()
+
+def D():
+    print("entering inside D")
+
+def E():
+    print("entering inside E")
+    F()
+
+def F():
+    print("entering inside F")
+    G()
+def G():
+    print("entering inside G")
+    H()
+
+def H():
+    print("entering inside H")
+    I()
+def I():
+    print("entering inside I")
+    J()
+
+def J():
+    print("entering inside J")
+
+def K():
+    print("entering inside K")
+    L()
+
+def L():
+    print("entering inside L")
+    M()
+
+def M():
+    print("entering inside M")
+    N()
+
+def N():
+    print("entering inside N")
+    O()
+
+def O():
+    print("entering inside O")
+
+def P():
+    print("entering inside D")
+    Q()
+def Q():
+    print("entering inside Q")
+    R()
+def R():
+    print("entering inside R")
+    S()
+def S():
+    print("entering inside S")
+    T()
+def T():
+    print("entering inside T")
+    Q()
+def Q():
+    print("entering inside Q")
+
+
+def main():
+    print("inside project main ")
+if __name__ == '__main__':
+    main()
